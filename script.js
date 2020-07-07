971646f09491afa0eb82b76e4534d3e49d0e5eab
Conrad <gobolos.konrad@gmail.com>
2020-07-07 19:40:10 +0200

    modify js and css

---
+++ script.js
@@ -0,0 +1,12 @@
+//nav
+$('.burgerIcon').hover(
+    
+    function(){
+        TweenMax.to('.burgerLine:first-child',0.2, {x: -10});
+        TweenMax.to('.burgerLine:last-child',0.2, {x: 10});
+},
+
+    function(){
+        TweenMax.to('.burgerLine:first-child',0.2, {x: 0});
+        TweenMax.to('.burgerLine:last-child',0.2, {x: 0});
+    });
